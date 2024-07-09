68e461e682ef79aaba61e41b2d058cbf362aae5e
Edanriell <Edanriell@gmail.com>
2024-07-09 14:56:35 +0300

    finished animations

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideClientHydration } from "@angular/platform-browser";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 import { routes } from "@pages/app.routes";
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+	providers: [
+		provideAnimationsAsync(),
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes),
+		provideClientHydration()
+	]
 };
    
--- Client/src/shared/ui/link/link.component.ts
+++ Client/src/shared/ui/link/link.component.ts
@@ -1,11 +1,36 @@
 import { Component, Input } from "@angular/core";
+import { animate, state, style, transition, trigger } from "@angular/animations";
 
 @Component({
 	selector: "app-shared-link",
 	standalone: true,
-	imports: [],
 	templateUrl: "./link.component.html",
-	styleUrl: "./link.component.less"
+	styleUrl: "./link.component.less",
+	animations: [
+		trigger("linkAnimation", [
+			state("default", style({
+				transform: "scale(1, 1)",
+				boxShadow: "none"
+			})),
+			state("hovered", style({
+				transform: "scale(1.1, 1.1)",
+				boxShadow: "0px 10px 20px 2px rgba(0, 0, 0, 0.05)"
+			})),
+			state("active", style({
+				transform: "scale(0.95, 0.95)",
+				boxShadow: "0px 10px 20px 2px rgba(0, 0, 0, 0.25)"
+			})),
+			transition("default <=> hovered", [
+				animate("0.6s cubic-bezier(0.165, 0.84, 0.44, 1)")
+			]),
+			transition("hovered <=> active", [
+				animate("0.6s cubic-bezier(0.165, 0.84, 0.44, 1)")
+			]),
+			transition("default <=> active", [
+				animate("0.6s cubic-bezier(0.165, 0.84, 0.44, 1)")
+			])
+		])
+	] 
 })
 export class LinkComponent {
 	@Input({ required: true }) linkHref = "#";
@@ -13,13 +38,58 @@ export class LinkComponent {
 	@Input() shape: "pill" | "" = "";
 	@Input() color: "lusty-lavender" | "acapulco-cliffs" = "lusty-lavender";
 
+	isHovered = false;
+	isActive = false;
+
 	get linkClasses(): string[] {
 		return [
-			'link',
+			"link",
 			`link--size-${this.size}`,
 			`link--shape--${this.shape}`,
 			`link--color--${this.color}`
 		];
+	}
+
+	get dynamicBackgroundColor() {
+		if (this.isHovered) {
+			if (this.color === "lusty-lavender") {
+				return {
+					backgroundColor: "#b18bdd"
+				}
+			} else if (this.color === "acapulco-cliffs") {
+				return {
+					backgroundColor: "#71c0d4"
+				}
+			}
+		}
+		return {};
+	}
+
+	onMouseEnter() {
+		this.isHovered = true;
+	}
+
+	onMouseLeave() {
+		this.isHovered = false;
+		this.isActive = false;
+	}
+
+	onMouseDown() {
+		this.isActive = true;
+	}
+
+	onMouseUp() {
+		this.isActive = false;
+	}
+
+	onTouchStart() {
+		this.isHovered = true;
+		this.isActive = true;
+	}
+
+	onTouchEnd() {
+		this.isHovered = false;
+		this.isActive = false;
 	}
 }
  
